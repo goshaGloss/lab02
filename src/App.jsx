f799ddc041b666af13f0a79df2032139eea8025e
goshaGloss <georgiy.chetkov@gmail.com>
2026-01-30 23:54:31 +0500

    init: base project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Layout from "./task1/Layout";
 
 function App() {
-  const items = [ { id: 1, name: "Apple" }, { id: 2, name: "Banana" },{ id: 3, name: "Cherry" } ]
   return (
     <Layout />
   );
