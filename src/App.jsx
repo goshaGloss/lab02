83fdc6a5748991c48ece4d1aadd3351cb6b36b0b
goshaGloss <georgiy.chetkov@gmail.com>
2026-01-30 23:09:52 +0500

    init: base react project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Layout from "./task1/Layout";
+
+function App() {
+  const items = [ { id: 1, name: "Apple" }, { id: 2, name: "Banana" },{ id: 3, name: "Cherry" } ]
+  return (
+    <Layout />
+  );
+}
+
+export default App;
